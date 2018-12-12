81177ba4d412706df436511f671e59f3cfad3b69
Microsocks <barclayethan@gmail.com>
2018-12-12 17:07:06 -0500

    Create xpbot.py

---
+++ xpbot.py
@@ -0,0 +1,23 @@
+import discord
+import asyncio
+import logging
+import string
+import random
+
+
+# Set these variables
+message = 'MESSAGE'
+email = 'LOGIN EMAIL'
+password = 'PASSWORD'
+channel = discord.Object(id='CHANNEL ID')
+
+
+client = discord.Client()
+async def run():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        await client.send_message(channel, message)
+        await asyncio.sleep(60)
+logging.basicConfig(level=logging.INFO)
+client.loop.create_task(run())
+client.run(email, password)
